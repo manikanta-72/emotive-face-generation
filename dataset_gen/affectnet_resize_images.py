4d243b7ef7a74e3e092c81551bd95a40ad1e71e9
Sai Vineeth <saivineeth1074@gmail.com>
2022-11-20 18:41:26 -0800

    Images resize

---
+++ dataset_gen/affectnet_resize_images.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+
+print("Python Program to print list the files in a directory.")
+rootdir = 'old_images'
+newrootdir = 'new_images'
+
+
+dirs = ["anger", "contempt", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
+for dir in dirs:
+    print(f"Files in the directory: {rootdir+ '/' + dir}")
+    if not os.path.exists(rootdir+ '/' + dir):
+        os.makedirs(rootdir+ '/' + dir)
+    files = os.listdir(rootdir+ '/' + dir)
+    files = [f for f in files if os.path.isfile(rootdir+ '/' + dir+'/'+f)] #Filtering only the files.
+
+    for fl in files:
+        print(fl)
+        img = cv2.imread(rootdir+ '/' + dir+'/'+fl, cv2.IMREAD_UNCHANGED)
+    
+        print('Original Dimensions : ',img.shape)
+        
+        dim = (64, 64)
+        
+        # resize image
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        
+        print('Resized Dimensions : ',resized.shape)
+        
+        #cv2.imshow("Resized image", resized)
+        if not os.path.exists(newrootdir+ '/' + dir):
+            os.makedirs(newrootdir+ '/' + dir)
+        cv2.imwrite(newrootdir + '/' + dir+ '/' + fl, resized)
